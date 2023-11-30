c97f39b0e1e53c4b6aff07904da605f4acdcc163
David Arustamian <david_arustamian@rntgroup.com>
2023-11-30 17:02:45 +0300

    update pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hw_pp',
-    version='0.0.4',
+    version='0.1.0',
     description="A small example package for iris prediction app",
     classifiers = [
         "Programming Language :: Python :: 3",
